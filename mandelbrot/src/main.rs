83b0558f3c99fe04d7e554bc9c070a71a56c244e
sémantique <121696947+4epheus@users.noreply.github.com>
2023-11-03 17:38:52 +0000

    Refactor fractal drawing to support Julia sets

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -4,13 +4,19 @@ use std::io::Write;
 use std::str::FromStr;
 use num_complex::Complex;
 use image::{ImageBuffer, Rgb};
+use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 
-fn compute_color(c: Complex<f64>, max_iter: u32) -> Rgb<u8> {
-    let mut z = Complex::new(0.0, 0.0);
+enum FractalType {
+    Mandelbrot,
+    Julia(Complex<f64>)
+}
+
+fn compute_color(z: Complex<f64>, c: Complex<f64>, max_iter: u32) -> Rgb<u8> {
     let mut i = 0;
-    while i < max_iter && z.norm() <= 2.0 {
-        z = z * z + c;
+    let mut w = z;
+    while i < max_iter && w.norm() <= 2.0 {
+        w = w * w + c;
         i += 1;
     }
     let color = if i == max_iter {
@@ -24,16 +30,25 @@ fn compute_color(c: Complex<f64>, max_iter: u32) -> Rgb<u8> {
     color
 }
 
-fn draw_mandelbrot(width: u32, height: u32, max_iter: u32, scale: f64) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn draw_fractal(width: u32, height: u32, max_iter: u32, scale: f64, fractal_type: FractalType) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let mut imgbuf = ImageBuffer::new(width, height);
     let (w, h) = (width as f64, height as f64);
+    let pb = ProgressBar::new((width * height) as u64);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} ({percent}%)")
+        .progress_chars("#>-"));
     imgbuf.enumerate_pixels_mut().par_bridge().for_each(|(x, y, pixel)| {
         let cx = (x as f64 - 0.5 * w) * scale / w;
         let cy = (y as f64 - 0.5 * h) * scale / h;
         let c = Complex::new(cx, cy);
-        let color = compute_color(c, max_iter);
+        let color = match fractal_type {
+            FractalType::Mandelbrot => compute_color(Complex::new(0.0, 0.0), c, max_iter),
+            FractalType::Julia(z) => compute_color(z, c, max_iter)
+        };
         *pixel = color;
+        pb.inc(1);
     });
+    pb.finish_with_message("done");
     imgbuf
 }
 
@@ -47,21 +62,38 @@ fn parse_resolution(resolution: &str) -> Option<(u32, u32)> {
     Some((width, height))
 }
 
+fn parse_complex_number(s: &str) -> Option<Complex<f64>> {
+    let parts: Vec<&str> = s.split(',').collect();
+    if parts.len() != 2 {
+        return None;
+    }
+    let re = f64::from_str(parts[0]).ok()?;
+    let im = f64::from_str(parts[1]).ok()?;
+    Some(Complex::new(re, im))
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 7 {
-        println!("Usage: {} <output_file> <width>x<height> <capture_width>x<capture_height> <max_iter> <scale>", args[0]);
+    if args.len() != 8 {
+        println!("Usage: {} <output_file> <width>x<height> <capture_width>x<capture_height> <max_iter> <scale> <fractal_type> <c>", args[0]);
         return Ok(());
     }
     let output_file = &args[1];
-let (width, height) = parse_resolution(&args[2]).ok_or("Invalid resolution")?;
-let (capture_width, capture_height) = parse_resolution(&args[3]).ok_or("Invalid capture size")?;
-let max_iter = u32::from_str(&args[4]).map_err(|_| "Invalid max_iter")?;
-let scale = f64::from_str(&args[5]).map_err(|_| "Invalid scale")?;
-let imgbuf = draw_mandelbrot(capture_width, capture_height, max_iter, scale);
-let resized = image::imageops::resize(&imgbuf, width, height, image::imageops::FilterType::Lanczos3);
-let mut file = File::create(output_file)?;
-resized.save_with_format(&mut file, image::ImageFormat::PNG)?;
-Ok(())
+    let (width, height) = parse_resolution(&args[2]).ok_or("Invalid resolution")?;
+    let (capture_width, capture_height) = parse_resolution(&args[3]).ok_or("Invalid capture size")?;
+    let max_iter = u32::from_str(&args[4]).map_err(|_| "Invalid max_iter")?;
+    let scale = f64::from_str(&args[5]).map_err(|_| "Invalid scale")?;
+    let fractal_type = match &args[6][..] {
+        "mandelbrot" => FractalType::Mandelbrot,
+        "julia" => {
+            let c = parse_complex_number(&args[7]).ok_or("Invalid complex number")?;
+            FractalType::Julia(c)
+        },
+        _ => return Err("Invalid fractal type".into())
+    };
+    let imgbuf = draw_fractal(capture_width, capture_height, max_iter, scale, fractal_type);
+    let resized = image::imageops::resize(&imgbuf, width, height, image::imageops::FilterType::Lanczos3);
+    let mut file = File::create(output_file)?;
+    resized.save_with_format(&mut file, image::ImageFormat::PNG)?;
+    Ok(())
 }
-```
